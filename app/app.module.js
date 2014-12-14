8e9e3eb660b1cd7000041ce672c15f9af6bf0f1f
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-12-14 02:28:28 -0500

    included staff module in main app, also moved some login to main apps sun function (setting up  state listener)

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,23 @@
-var app = angular.module('app', ['projectsModule']);
+var app = angular.module('app', ['projectsModule','staffModule']);
+// Include these models in main module
 var projectsModule = angular.module('projectsModule', ['ui.router','ngResource','ngSanitize']);
+var staffModule = angular.module('staffModule', ['ui.router','ngResource','ngSanitize']);
+
+app.run(['$rootScope','Project' , function($rootScope, Project){
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+		console.log("Going to state ->", toState.name);
+
+		var goingToProjectState = toState.name == "projects" || toState.name == "projectCreate" || toState.name == "projectDetail";
+		var goingToStaffState = toState.name == "staff";
+
+		if (goingToProjectState){
+			$rootScope.section = 'projects';
+		}
+		else if (goingToStaffState) {
+			$rootScope.section = 'staff';
+		}
+		else {
+			console.log("Where are you going?");
+		}
+	});
+}]);
