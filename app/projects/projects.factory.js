5ed626a70b557e148c0245971b3e483c0846daf2
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-12-15 00:17:30 -0500

    factories now link to server ip hoho

--- app/projects/projects.factory.js
+++ app/projects/projects.factory.js
@@ -1,7 +1,7 @@
 projectsModule.factory('Project', ['$http', function($http){
     return {
     	getList: function() {
-    		var projectUrl = "http://localhost:8000/api/v1/project/";
+    		var projectUrl = "http://104.236.6.55:8000/api/v1/project/";
 
     		var promise = $http({
     			url: projectUrl,
@@ -10,7 +10,7 @@ projectsModule.factory('Project', ['$http', function($http){
     		return promise;
     	},
     	getDetails: function(id) {
-    		var projectUrl = "http://localhost:8000/api/v1/project/" + id + "/";
+    		var projectUrl = "http://104.236.6.55:8000/api/v1/project/" + id + "/";
     		var promise = $http({
     			url: projectUrl,
     			method: "GET",
@@ -18,7 +18,7 @@ projectsModule.factory('Project', ['$http', function($http){
     		return promise;
     	},
     	getForm: function(id) {
-    		var formUrl = "http://localhost:8000/form-project/";
+    		var formUrl = "http://104.236.6.55:8000/form-project/";
     		var promise = $http({
     			url: formUrl,
     			method: "GET",
